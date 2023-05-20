42c513fa3e020f9a91d5597ee17a96a80df3fe25
Euisoon Ahn <euisoon.ahn@gmail.com>
2023-05-20 20:05:14 +0900

    fix the method used to get total data

--- stat_pivot.py
+++ stat_pivot.py
@@ -3,26 +3,39 @@ import pandas as pd
 from pathlib import Path
 
 output = Path("output")
+
+## 시도
+
 dfs = {
     file.stem: pd.read_csv(file, index_col=0, usecols=range(1, 8))
-    for file in output.glob("*.csv")
+    for file in output.glob(("[0-9]" * 6) + ".csv")
 }
 
-# print(dfs[0].합계)
-# print(dfs[0].sum()[1:])
-
 df_sido = pd.concat(
-    [df.합계.to_frame(name=key).transpose().sort_index() for key, df in dfs.items()]
-)
-df_use = pd.concat(
-    [
-        df.sum()[1:].to_frame(name=key).transpose().sort_index()
-        for key, df in dfs.items()
-    ]
-)
+    [df.합계.to_frame(name=key).transpose() for key, df in dfs.items()]
+).sort_index()
 
-print(df_sido)
-print(df_use)
-
+# print(df_sido)
 df_sido.to_csv(f"output/completion_sido.csv", encoding="utf-8-sig")
+
+
+## 전국
+dfs = {
+    file.stem[-6:]: pd.read_csv(file, index_col=0, usecols=range(1, 8))
+    for file in output.glob("total_" + ("[0-9]" * 6) + ".csv")
+}
+# print(list(dfs.keys()))
+
+df_use = pd.concat(
+    [df.iloc[:, 1:].rename(index={"전국": key}) for key, df in dfs.items()]
+).sort_index()
+
+print(df_use)
 df_use.to_csv(f"output/completion_use.csv", encoding="utf-8-sig")
+
+df_total = pd.concat(
+    [df.합계.to_frame(name=key).transpose() for key, df in dfs.items()]
+).sort_index()
+
+print(df_total)
+df_total.to_csv(f"output/completion_total.csv", encoding="utf-8-sig")
