2e752bd5d28c8e8594ce002ee0020fecd25ee479
Euisoon Ahn <euisoon.ahn@gmail.com>
2025-05-25 23:56:30 +0900

    remove old files

--- auri.py
+++ auri.py
@@ -1,8 +1,6 @@
-from pathlib import Path
-
-# import matplotlib.pyplot as plt
+# from pathlib import Path
 import pandas as pd
 
-# plt.style.use(Path("./auri.mplstyle"))
-
-pd.options.display.unicode.east_asian_width = True
+def init():
+    # plt.style.use(Path("./auri.mplstyle"))
+    pd.options.display.unicode.east_asian_width = True
